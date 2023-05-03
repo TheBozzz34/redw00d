baadb202b1481f80d8d01882e60f089349a427cc
Ethan James <lskywalker416@gmail.com>
2023-05-02 21:57:22 -0600

    commit 2

--- web/src/components/NavBar/NavBar.js
+++ web/src/components/NavBar/NavBar.js
@@ -1,5 +1,48 @@
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs'
+import { FaFire, FaPoo } from 'react-icons/fa'
+
 const SideBar = () => {
-  return <div>SideBar</div>
+  return (
+    <div className="sidebar">
+      <div className="sidebar-header">
+        <div className="sidebar-header-text">RedwoodJS</div>
+        <div className="sidebar-header-icon">
+          <BsPlus />
+        </div>
+      </div>
+      <div className="sidebar-menu">
+        <div className="sidebar-menu-item">
+          <div className="sidebar-menu-item-icon">
+            <FaFire />
+          </div>
+          <div className="sidebar-menu-item-text">Dashboard</div>
+        </div>
+        <div className="sidebar-menu-item">
+          <div className="sidebar-menu-item-icon">
+            <BsFillLightningFill />
+          </div>
+          <div className="sidebar-menu-item-text">Quick Start</div>
+        </div>
+        <div className="sidebar-menu-item">
+          <div className="sidebar-menu-item-icon">
+            <FaPoo />
+          </div>
+          <div className="sidebar-menu-item-text">Tutorial</div>
+        </div>
+        <Divider />
+        <div className="sidebar-menu-item">
+          <div className="sidebar-menu-item-icon">
+            <BsGearFill />
+          </div>
+          <div className="sidebar-menu-item-text">Settings</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const Divider = () => {
+  return <div className="sidebar-menu-divider"></div>
 }
 
 export default SideBar
